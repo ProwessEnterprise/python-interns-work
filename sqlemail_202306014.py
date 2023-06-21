ddd34ed801113b57c7751133cc57d49bf91034dc
saividhyagottipati <saividya.g@prowessenterprise.com>
2023-06-21 17:02:37 +0530

    sqlemail update

---
+++ sqlemail_202306014.py
@@ -0,0 +1,125 @@
+import pymysql.cursors
+from dotenv import load_dotenv
+import os
+
+class EmailDatabase:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        self.connection = pymysql.connect(host=self.host,
+                                          user=self.user,
+                                          password=self.password,
+                                          database=self.database,
+                                          cursorclass=pymysql.cursors.DictCursor)
+        self.cursor = self.connection.cursor()
+    
+    def create_table(self):
+        query = """CREATE TABLE IF NOT EXISTS emails(
+                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                   subject VARCHAR(255),
+                   body text)"""
+        self.cursor.execute(query)
+        self.connection.commit()
+   
+    def insert_emails(self, receiver, subject, body):
+        query = """INSERT INTO emails( receiver, subject, body)
+                VALUES(%s,%s,%s)"""
+        self.cursor.execute(query,( receiver, subject, body))
+        self.connection.commit()
+    
+    def get_all_emails(self):
+        query = "SELECT * FROM emails ORDER BY id DESC LIMIT 1"
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        emails = []
+        for row in result:
+            email = {
+            "id": row['id'],
+            "receiver": row['receiver'],
+            "subject": row['subject'],
+            "body": row['body']
+            }
+            emails.append(email)
+        return emails
+    
+    
+
+
+    
+    #def column_add(self):
+        query="""ALTER TABLE emails 
+               ADD column receiver VARCHAR(225) AFTER id"""
+        self.cursor.execute(query)
+        self.connection.commit()
+    
+    def delete_emails(self):
+        query= """DELETE FROM emails"""
+        self.cursor.execute(query)
+        self.connection.commit()
+    
+    def update_emails(self,id, receiver, subject, body):
+        query= """UPDATE emails 
+               SET receiver=%s, subject=%s, body=%s
+               WHERE id=%s"""
+        self.cursor.execute(query,(receiver, subject, body, id))
+        self.connection.commit()    
+    
+    #def auto_increment(self):
+        #query="""ALTER TABLE emails AUTO_INCREMENT=1"""
+        #self.cursor.execute(query)
+        #self.connection.commit()
+    
+    def disconnect(self):
+        if self.connection and self.cursor:
+            self.cursor.close()
+            self.connection.close()
+            self.cursor = None
+            self.connection = None
+def main():
+    load_dotenv()
+    host_name = os.environ['host_name']
+    user_name = os.environ['user_name']
+    database_password = os.environ['database_password']
+    database_name1 = os.environ['database_name1']
+
+    db = EmailDatabase(host_name, user_name, database_password, database_name1)
+    db.connect()
+    db.create_table()
+    
+    #subject= 'JUICE SALON MEMBERSHIP ACTIVATION'
+    discount= '20%'
+    body="""Dear Customers,
+
+Congratulations! JUICE SALON is glad to tell you that you just got your membership card! This membership card will let you have our best services on affordable prices. 
+Yes, you can have any service on straight ({} off) .
+Juice Salon thanks you to be our customer and in return, we honour you with our membership card for your next visit. 
+Avail the opportunity and enjoy our quality services as soon as possible.
+
+Wishing to see you again,
+JUICE SALON""".format(discount)
+
+    db.insert_emails(1,'gottipatisaividhya4280@gmail.com','AUTOMATED EMAILS','sending emails through sql')
+    #db.insert_emails(2,'sgottipa@gitam.in','AUTOMATED EMAILS','sending emails through sql')
+    
+    #db.column_add()
+    
+    emails = db.get_all_emails()
+    print("ALL EMAILS:")
+    print(emails)
+    
+    #db.update_emails(1, 'gottipatisaividhya4280@gmail.com', subject, body)
+
+    #db.auto_increment()
+
+    #db.delete_emails(4)
+
+    db.disconnect()
+
+if __name__ == '__main__':
+    main()
