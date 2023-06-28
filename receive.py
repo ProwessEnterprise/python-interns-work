7431c2b2e8edb41e55519dca7b99b177bf1176e0
saividhyagottipati <saividya.g@prowessenterprise.com>
2023-06-28 14:58:35 +0530

    Jsonupdate

--- receive.py
+++ receive.py
@@ -1,10 +1,11 @@
 import pika, sys, os
+import json
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='hello')
     def callback(ch, method, properties, body):
-        message = body.decode()
+        message = json.loads(body.decode())
         print(" [x] Received %r" % message)
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
     print(' [*] Waiting for messages. To exit press CTRL+C')                     
