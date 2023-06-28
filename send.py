43854bde64b6f48aa17f13037c9f4f3452f0e00d
saividhyagottipati <saividya.g@prowessenterprise.com>
2023-06-28 14:57:25 +0530

    Json update

--- send.py
+++ send.py
@@ -1,10 +1,20 @@
 import pika
+import json
+import pymysql.cursors
 
+name=input('Enter name')
+age=int(input('Enter age'))
+message = [name, age]
 connection =  pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='Hello World')
-print("[x] Sent 'Hello World'")
+                      body=json.dumps(message))
+body=json.dumps(message)
+result_type=type(body)
+message_type=type(message)
+print("type of message = ", message_type)
+print("type of result = ", result_type)
+print("[x] Sent data")
 connection.close()
